a724cce67ffc79d5474c6854db3fccc3a4e466f5
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-03-31 21:35:05 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 #include "Automata.h"
 
-int main()
-{
-	Automata coffeMachine;
-	Work_Automata(coffeMachine);
-}
-
 void Work_Automata(Automata CoffeMachine) {
 	CoffeMachine.on();
 	CoffeMachine.coin(8);
 	CoffeMachine.choice(3);
 	CoffeMachine.cancel();
 	CoffeMachine.coin(12);
-	CoffeMachine.check(3);
+	CoffeMachine.choice(3);
 	CoffeMachine.cook(3);
 	CoffeMachine.finish();
 	CoffeMachine.off();
+}
+
+int main()
+{
+	Automata coffeMachine;
+	Work_Automata(coffeMachine);
+	
 }
