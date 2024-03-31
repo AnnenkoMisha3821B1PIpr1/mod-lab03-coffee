72ca54091fe663183e3e7246ccb78a14210307fa
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-03-31 23:10:33 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -6,30 +6,30 @@
 using namespace std;
 
 enum STATES {
-	OFF,
-	WAIT,
-	ACCEPT,
-	CHECK,
-	COOK
+OFF,
+WAIT,
+ACCEPT,
+CHECK,
+COOK
 };
 
 class Automata {
-public:
-	Automata();
-	~Automata();
-	void on();
-	void off();
-	void coin(int Money);
-	void getMenu();
-	STATES getState();
-	void choice(int Choice);
-	void check(int Check);
-	void cancel();
-	void cook(int num);
-	void finish();
-private:
-	int cash;
-	string* menu;
-	int* prices;
-	STATES state;
+ public:
+Automata();
+~Automata();
+void on();
+void off();
+void coin(int Money);
+void getMenu();
+STATES getState();
+void choice(int Choice);
+void check(int Check);
+void cancel();
+void cook(int num);
+void finish();
+ private:
+int cash;
+string* menu;
+int* prices;
+STATES state;
 };
